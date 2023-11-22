20fb21a95874a1507aab1b34065ec68faf658f97
aptaclino1 <107854593+aptaclino1@users.noreply.github.com>
2023-11-22 21:23:41 +0800

    new label

--- Git_Hub_Final_Test/ViewController.swift
+++ Git_Hub_Final_Test/ViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var testLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+     
+        testLabel.text = "Hello world!"
+        testLabel.font = .boldSystemFont(ofSize: 30)
     }
 
 
